498a1fcae3f01d3923be245bf9c5ea54c2b7674b
Christopher Hern <christopher.hern@oracle.com>
2025-12-16 15:13:37 -0600

    fix: unused import and var

--- src/runner/main.go
+++ src/runner/main.go
@@ -2,12 +2,10 @@ package main
 
 import (
 	"fmt"
-	"os"
 	"github.com/montybeatnik/arista-lab/laber/pkgs/arista"
 )
 
 func main() {
-	cmds := []string{"show bgp summary"}
 	url := "https://172.20.20.9/command-api"
 	client := arista.NewEosClient(url)
 	bgpEvpnSummaryResp, err := client.BGPSummary()
