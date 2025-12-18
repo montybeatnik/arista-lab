9d3267b4e18d5e5c8dfddaedb70564a5dcdcc654
Christopher Hern <christopher.hern@oracle.com>
2025-12-18 09:24:47 -0600

    debug: auth

--- config_generator/infrastructure/arista_device_connector.py
+++ config_generator/infrastructure/arista_device_connector.py
@@ -15,13 +15,17 @@ class AristaDeviceConnector:
             "use_keys": False,
             "allow_agent": False
         }
-        with ConnectHandler(**device_config) as net_connect:
-            output = net_connect.send_command("show ip int lo0")
-            # Parse the output to extract the IP address
-            for line in output.splitlines():
-                if "IP Address" in line:
-                    loopback_ip = line.split(":")[1].strip().split("/")[0]
-                    return loopback_ip
+        try:
+            with ConnectHandler(**device_config) as net_connect:
+                output = net_connect.send_command("show ip int lo0")
+                # Parse the output to extract the IP address
+                for line in output.splitlines():
+                    if "IP Address" in line:
+                        loopback_ip = line.split(":")[1].strip().split("/")[0]
+                        return loopback_ip
+        except Exception as e:
+            print(device)
+            print(f"failed to connect: {e}")
 
     def apply_configuration(self, device, config):
         # Implement the logic to apply the configuration to the device
