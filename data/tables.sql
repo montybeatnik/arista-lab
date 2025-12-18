50693918b24cdb9ef83c0f9297b47ceaab658214
Christopher Hern <christopher.hern@oracle.com>
2025-12-18 09:12:47 -0600

    fix: creds

--- data/tables.sql
+++ data/tables.sql
@@ -2,5 +2,7 @@ CREATE TABLE devices (
     id SERIAL PRIMARY KEY,
     ip_address VARCHAR(15) UNIQUE NOT NULL,
     hostname VARCHAR(255),
-    loopback_ip VARCHAR(15)
+    loopback_ip VARCHAR(15),
+    username VARCHAR(15),
+    password VARCHAR(15)
 );
