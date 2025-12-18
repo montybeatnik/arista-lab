03e7382c832cc5deff7dff77155a23c3ed3081d1
Christopher Hern <christopher.hern@oracle.com>
2025-12-18 09:51:49 -0600

    refactor: using api for cfgs

--- config_generator/infrastructure/arista_device_connector.py
+++ config_generator/infrastructure/arista_device_connector.py
@@ -1,4 +1,6 @@
 from netmiko import ConnectHandler
+import requests
+import json
 
 class AristaDeviceConnector:
     def __init__(self):
@@ -26,17 +28,44 @@ class AristaDeviceConnector:
         except Exception as e:
             print(f"failed to connect: {e}")
 
-    def apply_configuration(self, device, config):
-        # Implement the logic to apply the configuration to the device
-        device_config = {
-            "device_type": "arista_eos",
-            "ip": device.ip_address,
-            "username": device.username,
-            "password": device.password,
-            "use_keys": False,
-            "allow_agent": False,
-            "conn_timeout": 10,
-            "read_timeout": 30 
+    # def apply_configuration(self, device, config):
+    #     # Implement the logic to apply the configuration to the device
+    #     device_config = {
+    #         "device_type": "arista_eos",
+    #         "ip": device.ip_address,
+    #         "username": device.username,
+    #         "password": device.password,
+    #         "use_keys": False,
+    #         "allow_agent": False,
+    #         "conn_timeout": 10,
+    #         "read_timeout": 30 
+    #     }
+    #     with ConnectHandler(**device_config) as net_connect:
+    #         net_connect.send_config_set(config.splitlines())
+
+    def apply_configuration(device, config):
+        url = f"https://{device.ip_address}/command-api"
+        auth = (device.username, device.password)
+        headers = {"Content-Type": "application/json"}
+
+        commands = config.splitlines()
+        payload = {
+            "jsonrpc": "2.0",
+            "method": "runCmds",
+            "params": {
+                "format": "json",
+                "timestamps": False,
+                "autoComplete": False,
+                "expandAliases": False,
+                "cmds": commands,
+                "version": 1
+            },
+            "id": "1"
         }
-        with ConnectHandler(**device_config) as net_connect:
-            net_connect.send_config_set(config.splitlines())
+
+        response = requests.post(url, auth=auth, headers=headers, data=json.dumps(payload), verify=False)
+
+        if response.status_code == 200:
+            print(f"Configuration applied successfully to {device.ip_address}")
+        else:
+            print(f"Failed to apply configuration to {device.ip_address}: {response.text}")
