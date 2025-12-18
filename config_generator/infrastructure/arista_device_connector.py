bad1efe7c34f263f311c8421b8f3d13e12b878a7
Christopher Hern <christopher.hern@oracle.com>
2025-12-18 10:10:16 -0600

    debug: cfg apply

--- config_generator/infrastructure/arista_device_connector.py
+++ config_generator/infrastructure/arista_device_connector.py
@@ -70,6 +70,6 @@ class AristaDeviceConnector:
 
         if response.status_code == 200:
             print(f"Configuration applied successfully to {device.ip_address}")
-            print(f"{response=}")
+            print(f"##### DEBUG #####{response.content=}")
         else:
             print(f"Failed to apply configuration to {device.ip_address}: {response.text}")
